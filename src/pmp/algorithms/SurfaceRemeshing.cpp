785cbd748d109a7870a0df0bd3a218e9fcdf932c
twennemann <twennemann94@googlemail.com>
2023-03-20 18:47:18 +0100

    Check for channel unit

--- src/pmp/algorithms/SurfaceRemeshing.cpp
+++ src/pmp/algorithms/SurfaceRemeshing.cpp
@@ -185,6 +185,14 @@ void SurfaceRemeshing::preprocessing(std::string ear,
             min_edge_length_ *= 0.001;
             approx_error_ *= 0.001;
         }
+        if (unit = "m" && channel_left > 10)
+        {
+            channel_left *= 0.001;
+        }
+        if (unit = "m" && channel_right > 10)
+        {
+            channel_right *= 0.001;
+        } 
 
         if (ear != "none")
         {
